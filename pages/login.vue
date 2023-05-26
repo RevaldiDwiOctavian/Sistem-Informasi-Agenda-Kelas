3081712b14aa2cb210512b339b5196151ec6b624
Revaldi Dwi Octavian <revaldidwioctavian@gmail.com>
2023-05-26 23:24:28 +0700

    Init project with auth

---
+++ pages/login.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Login form</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <form ref="form" @submit.prevent="login">
+                  <v-text-field
+                    v-model="email"
+                    name="email"
+                    label="Email"
+                    type="text"
+                    placeholder="Email"
+                    required
+                  ></v-text-field>
+
+                  <v-text-field
+                    v-model="password"
+                    name="password"
+                    label="Password"
+                    type="password"
+                    placeholder="password"
+                    required
+                  ></v-text-field>
+                  <v-btn
+                    type="submit"
+                    class="mt-4"
+                    color="primary"
+                    value="log in"
+                    >Login</v-btn
+                  >
+                </form>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  layout: 'auth',
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    async login() {
+      await this.$auth.loginWith('laravelSanctum', {
+        data: {
+          email: this.email,
+          password: this.password,
+        },
+      })
+
+      this.$router.push('/')
+    },
+  },
+}
+</script>
